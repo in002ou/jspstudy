52a56b71aa4493fb8049427d057b9c9f77bb3c7e
in002ou <in002ou@gmail.com>
2023-04-07 17:49:01 +0900

    23/04/07

---
+++ 06_Ajax/src/main/java/service/MemberRemoveService.java
@@ -0,0 +1,36 @@
+package service;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import domain.Member;
+import repository.MemberDAO;
+
+public class MemberRemoveService implements IMemberService {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		String strMemberNo = request.getParameter("memberNo");
+		int memberNo = Integer.parseInt(strMemberNo);
+		
+		int deleteResult = MemberDAO.getInstance().deleteMember(memberNo);
+		
+		response.setContentType("application/json; charset=UTF-8");
+		
+		JSONObject obj = new JSONObject();
+		obj.put("deleteResult", deleteResult);
+		
+		PrintWriter out = response.getWriter();
+		out.println(obj.toString());
+		out.flush();
+		out.close();
+		
+		
+	}
+
+}
